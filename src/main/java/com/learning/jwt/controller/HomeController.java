6a005bdee444d515ae1837075f0d2413f226521e
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-19 00:58:47 +0500

    Added HomeController within controller package

---
+++ src/main/java/com/learning/jwt/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.learning.jwt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/welcome")
+    public String welcome(){
+        String text = "this is private page"
+                text+="this page is not allowed to unautenticated users ";
+        return text;
+    }
+}
