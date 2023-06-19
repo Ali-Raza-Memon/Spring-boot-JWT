7170cda6cc0afc5db9d7c13e877b9c9543586708
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-19 10:37:41 +0500

    Data is saved in User entitiy whithin CustomUser for understanding JWT instead in database

--- src/main/java/com/learning/jwt/service/CustomUserDetailsService.java
+++ src/main/java/com/learning/jwt/service/CustomUserDetailsService.java
@@ -1,12 +1,27 @@
 package com.learning.jwt.service;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        if(username.equals("Durgesh")){
+            return new User("Durgesh","Durgesh",new ArrayList<>());
+        }else{
+            throw new UsernameNotFoundException("User not found !");
+        }
+
+
     }
+
+
 }
