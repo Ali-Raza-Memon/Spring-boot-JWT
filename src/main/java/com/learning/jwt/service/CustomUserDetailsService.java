93d0fcc46efa185377f7674bc7f0195af388a654
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-19 10:16:00 +0500

    problem resolved! we had to just override the method while implements interface

--- src/main/java/com/learning/jwt/service/CustomUserDetailsService.java
+++ src/main/java/com/learning/jwt/service/CustomUserDetailsService.java
@@ -1,6 +1,12 @@
 package com.learning.jwt.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class CustomUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
