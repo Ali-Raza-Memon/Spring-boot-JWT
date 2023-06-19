65e1640c773bc8bdddc6a746034dceca10082d4b
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-19 13:54:58 +0500

    Welcome api is not working

--- src/main/java/com/learning/jwt/controller/HomeController.java
+++ src/main/java/com/learning/jwt/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+
     @GetMapping("/welcome")
     public String welcome(){
         String text = "this is private page ";
