6cf9cb56e9fb09185393e00fb7efa266c74d0580
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-19 10:15:25 +0500

    problem resolved! we had to just override the method while implements interface

--- src/main/java/com/learning/jwt/config/MySecurityConfig.java
+++ src/main/java/com/learning/jwt/config/MySecurityConfig.java
@@ -1,6 +1,10 @@
 package com.learning.jwt.config;
 
+import com.learning.jwt.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +12,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService);
+    }
 }
---
+++ src/main/java/com/learning/jwt/service/CustomUserDetailsService.java
@@ -0,0 +1,6 @@
+package com.learning.jwt.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class CustomUserDetailsService implements UserDetailsService {
+}
